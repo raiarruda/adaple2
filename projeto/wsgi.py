aeb4dc7af41ed17bc62bcd374d78ffeac38d2856
raissa <raiarruda.raa@gmauil.com>
2018-03-09 13:01:51 -0300

    wsgi atualizado pra pyhtonanywhere

--- projeto/wsgi.py
+++ projeto/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for projeto project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/raiarruda/adaple'  # use your own PythonAnywhere username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'projeto.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projeto.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
